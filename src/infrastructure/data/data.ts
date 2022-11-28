db9781d8242a379582b86b782e319b60e637ad39
JaeWang Lee <jnso5072@outlook.kr>
2022-11-28 14:47:52 +0900

    Add mock data

---
+++ src/infrastructure/data/data.ts
@@ -0,0 +1,55 @@
+import type { VideoItemModel } from './models';
+
+export const mostViewdData: VideoItemModel[] = [
+  {
+    id: '1',
+    title: '1',
+    uploaderName: '1',
+    videoUrl: 'https://www.naver.com',
+    thumbnailPath: '/Rectangle 35.png',
+    createdAt: new Date(),
+  },
+  {
+    id: '2',
+    title: '2',
+    uploaderName: '2',
+    videoUrl: 'https://www.naver.com',
+    thumbnailPath: '/Rectangle 36.png',
+    createdAt: new Date(),
+  },
+  {
+    id: '3',
+    title: '3',
+    uploaderName: '3',
+    videoUrl: 'https://www.naver.com',
+    thumbnailPath: '/Rectangle 37.png',
+    createdAt: new Date(),
+  },
+];
+
+export const newData: VideoItemModel[] = [
+  {
+    id: '1',
+    title: '1',
+    uploaderName: '1',
+    videoUrl: 'https://www.naver.com',
+    thumbnailPath: '/R1.png',
+    createdAt: new Date(),
+  },
+  {
+    id: '2',
+    title: '2',
+    uploaderName: '2',
+    videoUrl: 'https://www.naver.com',
+    thumbnailPath: '/R2.png',
+    createdAt: new Date(),
+  },
+  {
+    id: '3',
+    title: '3',
+    uploaderName: '3',
+    videoUrl: 'https://www.naver.com',
+    thumbnailPath: '/R3.png',
+    createdAt: new Date(),
+  },
+];
