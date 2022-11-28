f38a10fb2cd16fd46684cd39abe3d9d4780ce804
JaeWang Lee <jnso5072@outlook.kr>
2022-11-28 14:28:30 +0900

    Add video detail page

---
+++ src/pages/detail/[id].tsx
@@ -0,0 +1,12 @@
+import VideoDetail from '@presentation/features/videoDetail';
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+
+const VideoDetailPage: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return <VideoDetail videoId={id as string} />;
+};
+
+export default VideoDetailPage;
