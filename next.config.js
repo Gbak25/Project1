d7e20e1a5ee8271f2e18d3a28f0456d48eb97a97
JaeWang Lee <jnso5072@outlook.kr>
2023-01-21 01:05:00 +0900

    Support css alias

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+const path = require('path');
 const withInterceptStdout = require('next-intercept-stdout');
 
 module.exports = withInterceptStdout({
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@styles': path.resolve(__dirname, 'src/application/styles'),
+    };
+    return config;
+  },
 }, (text) => (text.includes('Duplicate atom key') ? '' : text));
